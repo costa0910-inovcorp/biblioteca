55104f4a0fd63218f65d6a05fb0a58d944d41197
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2025-02-10 20:14:24 +0000

    Add spatie Middleware for permissions and user roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Enums\RolesEnum;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,16 +18,39 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        $citizenPermissions = ['request books'];
+
+        $adminPermissions = ['manage books', 'view all requests', 'confirm reception'];
+        $allPermissions = array_merge($adminPermissions, $citizenPermissions);
+
+        $this->CreatePermissions($allPermissions);
+        $this->CreateRolesWithPermissions(RolesEnum::ADMIN, $allPermissions);
+        $this->CreateRolesWithPermissions(RolesEnum::CITIZEN, $citizenPermissions);
+
+        $user = User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => bcrypt('password'),
         ]);
+
+        $user->assignRole(RolesEnum::ADMIN);
 
         for ($i = 0; $i < 30; $i++) {
             $this->call(PublisherSeeder::class);
             $this->call(BookSeeder::class);
             $this->call(AuthorSeeder::class);
         }
+    }
+
+    protected function CreatePermissions(array $permissions): void
+    {
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+
+    protected function CreateRolesWithPermissions(RolesEnum $role, array $permissions): void {
+        $newRole = Role::create(['name' => $role]);
+        $newRole->syncPermissions($permissions);
     }
 }
