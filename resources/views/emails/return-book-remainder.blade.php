b1e6887e3958e12349b668bddd6050bc9593925e
costa0910-inovcorp <armando.costa@trainee.inovcorp.com>
2025-02-14 15:19:21 +0000

    fix: cover image book on remainder email

--- resources/views/emails/return-book-remainder.blade.php
+++ resources/views/emails/return-book-remainder.blade.php
@@ -3,10 +3,10 @@
         Reminder that's date to return book it's tomorrow
     </x-slot:title>
     <h2 style="color: #333; font-size: 20px;">Hello {{  $requestDetails->user_name }},</h2>
-    <p style="color: #555; font-size: 16px;">This email it's to remainder that's tomorrow {{ $requestDetails->predicted_return_date  }} it's the due date to return the book included below.</p>
+    <p style="color: #555; font-size: 16px;">This email it's to let you that tomorrow {{ $requestDetails->predicted_return_date  }} it's the due date to return the book included below.</p>
 
     <div style="text-align: center; margin: 20px 0;">
         <p>{{ $requestDetails->book->name }}</p>
-        <img src="{{ asset($requestDetails->cover_image) }}" alt="Book Cover" style="max-width: 100%; height: auto; border-radius: 5px;">
+        <img src="{{ asset($requestDetails->book->cover_image) }}" alt="Book Cover" style="max-width: 100%; height: auto; border-radius: 5px;">
     </div>
 </x-emails-template>
